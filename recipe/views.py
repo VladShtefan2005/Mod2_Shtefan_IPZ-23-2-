87fb1b8b2a3fdb680f57ae0ecb49dd7381897b59
VladShtefan1 <shtefanvlad@knu.ua>
2024-05-31 13:07:14 +0300

    corrected views

--- recipe/views.py
+++ recipe/views.py
@@ -4,4 +4,10 @@ from .models import Category
 
 def main(request):
     categories = Category.objects.all()
+    if not categories:
+        categories = [
+            {'name': 'Example Category 1'},
+            {'name': 'Example Category 2'},
+            {'name': 'Example Category 3'}
+        ]
     return render(request, 'main.html', context={'categories': categories})
