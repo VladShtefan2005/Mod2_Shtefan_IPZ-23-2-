dcfae201cb3ed36755a761b90cfb91a9442fb2f7
VladShtefan1 <shtefanvlad@knu.ua>
2024-05-31 12:47:38 +0300

    added file .env

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -9,11 +9,31 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Ініціалізація environ
+env = environ.Env(
+    # Налаштування стандартних значень (опційно)
+    DEBUG=(bool, False)
+)
+
+# Читання .env файлу
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Використання змінних з .env
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+DATABASES = {
+    'default': env.db(),
+}
+
+
 
 
 # Quick-start development settings - unsuitable for production
